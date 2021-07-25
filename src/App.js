574384d6d3be50e92460dd951685125938594e5c
Primayuda <aprita.primayuda@gmail.com>
2021-07-25 21:15:25 +0700

    First Draft

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [height, setHeight] = useState(0);
+  let [weight, setWeight] = useState(0);
+  let tempBmi = weight/(height*height);
+  let bmi = !isNaN(tempBmi) ? tempBmi.toFixed(2) : '__';
+  let condition = '__'
+  if (bmi >= 25.00) {
+    condition = 'Unhealty'
+  } else if (bmi !== '__'){
+    condition = 'Healthy'
+  }
+  
+
+  function handleChangeHeight(e) {
+    setHeight(height = parseFloat(e.target.value));
+    // console.log(height);
+  }
+  function handleChangeWeight(e) {
+    setWeight(weight = parseFloat(e.target.value));
+    // console.log(weight);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>BMI Calculator</h1>
+      <label>Height (m): <input onChange={handleChangeHeight} /></label>
+      <label>Weight (kg): <input onChange={handleChangeWeight} /></label>
+      
+      <p>BMI : {bmi} This is considered <strong>{condition}</strong></p>
       </header>
     </div>
   );
